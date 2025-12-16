28a331bb25b094e79f805f92792cda94c8d610e7
Ian Qvist <qvistian@gmail.com>
2025-12-16 15:14:35 +0100

    Add better error messages when working with corrupt winpe files

--- Src/FastCodeSign/Internal/WinPe/WinPeContext.cs
+++ Src/FastCodeSign/Internal/WinPe/WinPeContext.cs
@@ -9,6 +9,9 @@ internal sealed class WinPeContext : IContext
     {
         // Docs: https://upload.wikimedia.org/wikipedia/commons/1/1b/Portable_Executable_32_bit_Structure_in_SVG_fixed.svg
 
+        if (data.Length < 64)
+            throw new InvalidFileException("The file is too small to contain a valid DOS header.");
+
         ushort dosSignature = ReadUInt16LittleEndian(data);
 
         if (dosSignature != 0x5A4D) //MZ
@@ -17,19 +20,32 @@ internal sealed class WinPeContext : IContext
         // DOS header: Read e_lfanew, which is the pointer to the COFF header
         uint coffHeaderOffset = ReadUInt32LittleEndian(data[60..]);
 
+        if (coffHeaderOffset > (uint)(data.Length - 4))
+            throw new InvalidFileException("The file has an invalid COFF header offset.");
+
         // COFF header: Read the PE signature
         uint peSignature = ReadUInt32LittleEndian(data[(int)coffHeaderOffset..]);
 
         if (peSignature != 0x00004550) // "PE\0\0"
             throw new InvalidFileException("The file has an invalid PE signature.");
 
+        if (coffHeaderOffset + 24 > (uint)data.Length)
+            throw new InvalidFileException("The COFF header is truncated.");
+
         ushort numberOfSections = ReadUInt16LittleEndian(data[(int)(coffHeaderOffset + 6)..]);
         ushort sizeOfOptionalHeader = ReadUInt16LittleEndian(data[(int)(coffHeaderOffset + 20)..]);
 
         uint optionalHeaderOffset = coffHeaderOffset + 24;
+
+        if (optionalHeaderOffset + sizeOfOptionalHeader > (uint)data.Length)
+            throw new InvalidFileException("The optional header is truncated.");
+
         ushort magic = ReadUInt16LittleEndian(data[(int)optionalHeaderOffset..]);
 
         uint sectionTableOffset = optionalHeaderOffset + sizeOfOptionalHeader;
+
+        if (sectionTableOffset > (uint)data.Length)
+            throw new InvalidFileException("The section table offset is beyond the end of the file.");
 
         // Read PE sections
         List<PeSection> sections = new List<PeSection>(numberOfSections);
@@ -38,12 +54,18 @@ internal sealed class WinPeContext : IContext
         {
             uint sh = sectionTableOffset + (i * 40); //40 = section header size
 
+            if (sh + 40 > (uint)data.Length)
+                throw new InvalidFileException("The section table is truncated.");
+
             uint sizeOfRawData = ReadUInt32LittleEndian(data[((int)sh + 16)..]);
             uint pointerToRawData = ReadUInt32LittleEndian(data[((int)sh + 20)..]);
 
             if (sizeOfRawData > 0)
                 sections.Add(new PeSection(sizeOfRawData, pointerToRawData));
         }
+
+        if (optionalHeaderOffset + 64 > (uint)data.Length)
+            throw new InvalidFileException("The optional header is truncated.");
 
         uint sizeOfHeaders = ReadUInt32LittleEndian(data[((int)optionalHeaderOffset + 60)..]);
 
@@ -56,10 +78,16 @@ internal sealed class WinPeContext : IContext
         // Find the offset to the security data directory (contains the authenticode certificates)
         uint dataDirOffset = (uint)(coffHeaderOffset + (magic == 0x10b ? 120 : 136));
 
+        if (sizeOfOptionalHeader < dataDirOffset - optionalHeaderOffset + 8 || dataDirOffset + 8 > (uint)data.Length)
+            throw new InvalidFileException("The data directory is truncated.");
+
         // Data Directory is a set of (PVA + Size) which is 8 bytes in total.
         uint securityDirOffset = dataDirOffset + (4 * 8); // entry #4
         uint securityVirtualAddress = ReadUInt32LittleEndian(data[(int)securityDirOffset..]);
         uint securitySize = ReadUInt32LittleEndian(data[(int)(securityDirOffset + 4)..]);
+
+        if (securityVirtualAddress != 0 && (securityVirtualAddress > (uint)data.Length || securitySize > (uint)data.Length || securityVirtualAddress + securitySize > (uint)data.Length))
+            throw new InvalidFileException("The security directory points outside the file.");
 
         return new WinPeContext
         {
